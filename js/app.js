d5648a5c0b44531ea492d6dc41f6553bb892f86e
Martin Goldman <mgoldman@skytap.com>
2016-08-02 12:08:08 -0400

    guessing

--- js/app.js
+++ js/app.js
@@ -1,17 +1,64 @@
+var numGuesses, guesses, answer;
 
 $(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+  newGame();
 
-  	});
+  clearAndFocusGuessBox();
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+  $('.new').click(function(){
+    newGame();
+  });
+
+  $('form').submit(function() {
+    guess = parseInt($('#userGuess').val());
+    if (!isNaN(guess)) {
+      handleGuess(guess);
+    }
+
+    clearAndFocusGuessBox();
+    return false;
+  });
+
+	$(".what").click(function(){
+  	$(".overlay").fadeIn(1000);
+	});
+
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
 
 });
 
+function clearAndFocusGuessBox() {
+  $('#userGuess').val('');
+  $('#userGuess').focus();
+}
 
+function newGame() {
+  resetGuesses();
+  generateAnswer();
+}
+
+function generateAnswer() {
+  answer = Math.floor(Math.random() * 100) + 1;  
+}
+
+function resetGuesses() {
+  setNumGuesses(0);
+  guesses = [];
+}
+
+function setNumGuesses(num) {
+  numGuesses = num;
+  $('#count').text(num);
+}
+
+function addGuessToList(guess) {
+  guesses.push(guess);
+  $('.guessBox').append('<li>' + guess + '</li>');
+}
+
+function handleGuess(guess) {
+  addGuessToList(guess);
+  setNumGuesses(numGuesses+1);
+}
