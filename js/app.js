6283f1f741ac1b9cee78078de13f28e64bf6298f
Martin Goldman <mgoldman@skytap.com>
2016-08-03 17:59:23 -0400

    implement game

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@ var numGuesses, guesses, answer;
 $(document).ready(function(){
   newGame();
 
-  clearAndFocusGuessBox();
-
   $('.new').click(function(){
     newGame();
   });
@@ -35,18 +33,14 @@ function clearAndFocusGuessBox() {
 }
 
 function newGame() {
-  resetGuesses();
-  generateAnswer();
-}
-
-function generateAnswer() {
-  answer = Math.floor(Math.random() * 100) + 1;  
-}
-
-function resetGuesses() {
   setNumGuesses(0);
   guesses = [];
+  answer = Math.floor(Math.random() * 100) + 1;  
+  $('h2#feedback').text('Make your Guess!');
+  $('.guessBox').empty();
+  clearAndFocusGuessBox();  
 }
+
 
 function setNumGuesses(num) {
   numGuesses = num;
@@ -59,6 +53,32 @@ function addGuessToList(guess) {
 }
 
 function handleGuess(guess) {
-  addGuessToList(guess);
-  setNumGuesses(numGuesses+1);
+  if (guesses.indexOf(guess) === -1) {
+    addGuessToList(guess);
+    setNumGuesses(numGuesses+1);
+    feedback = guessFeedback(guess);
+    $('h2#feedback').text(feedback);    
+  }
+}
+
+function guessFeedback(guess) {
+  feedback = '';
+
+  difference = Math.abs(guess-answer);
+
+  if (difference >= 50) {
+    feedback = 'ice cold';
+  } else if (difference >= 30 && difference <= 49) {
+    feedback = 'cold';
+  } else if (difference >= 20 && difference <= 29) {
+    feedback = 'warm';
+  } else if (difference >= 10 && difference <= 19) {
+    feedback = 'hot';
+  } else if (difference >= 1 && difference <= 9) {
+    feedback = 'very hot';
+  } else if (difference == 0) {
+    feedback = "You Win! Press 'New Game' to play again";
+  }
+
+  return feedback;
 }
